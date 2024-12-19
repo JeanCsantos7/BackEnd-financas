23e2765f83b7303d97123e5dd6ad504b59911dd2
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-18 22:49:13 -0300

    boraa

--- app.ts
+++ app.ts
@@ -5,7 +5,11 @@ import Routes from './Routes';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: 'https://malandragem4.netlify.app',
+    }),
+);
 app.use(Routes);
 app.get('/', (req, res) => {
     res.send('Hello World');
