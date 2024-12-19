1caff2e62c33503c75ee6b970431b85232797bc7
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-18 22:55:19 -0300

    aac

--- app.ts
+++ app.ts
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: 'https://malandragem4.netlify.app',
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+        allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
     }),
 );
 app.use(Routes);
