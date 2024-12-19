ac1ce20ec876a919ceefc849fdc292a42f7d74de
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-18 22:35:50 -0300

    vergonha

--- Routes.ts
+++ Routes.ts
@@ -7,6 +7,9 @@ const Routes = Router();
 Routes.post('/adicionarReceita', ControllerReceitas.createReceitas);
 Routes.post('/adicionarDespesa', ControllerDespesas.createDespesas);
 Routes.get('/receitas', ControllerReceitas.findReceitas);
+Routes.get('/teste', (req, res) => {
+    res.send('rota teste');
+});
 Routes.get('/despesas', ControllerDespesas.findDespesas);
 Routes.get('/totalDespesas', ControllerDespesas.totalDespesas);
 Routes.get('/totalReceitas', ControllerReceitas.totalReceitas);
