2e0365f4cc09a786db19e8fb0a9de20b971dff97
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-20 22:45:38 -0300

    sandy e junior

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import app from './app';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado na porta: http://localhost:${PORT}`);
+    console.log(
+        `Servidor iniciado na porta: http://controle-financeiro-backend-production:${PORT}`,
+    );
 });
