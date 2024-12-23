a90e2d6f7432edb206846b67347c3624d74c4320
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-23 20:53:37 -0300

    alaal

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.listen(PORT, () => {
     console.log(
