b34896877965b92bf1de5586e5b863eba3095775
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-23 21:01:52 -0300

    jjkkk

--- server.ts
+++ server.ts
@@ -1,10 +1,8 @@
 import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(
-        `Servidor iniciado na porta: http://controle-financeiro-backend-production:${PORT}`,
-    );
+    console.log(`Servidor iniciado na porta: http://localhost:${PORT}`);
 });
