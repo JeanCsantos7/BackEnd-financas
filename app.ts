0d937bbf30141dedf7b423dc29643c6dcd4df107
Jean Carlos <126731254+JeanCsantos7@users.noreply.github.com>
2024-12-23 22:35:18 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -6,12 +6,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://controlefinanceiro7.netlify.app",
+    origin: "https://controlefinanceiro7.netlify.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-     res.header('Access-Control-Allow-Origin', 'https://controlefinanceiro7.netlify.app'),
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'),
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'),
+   allowedHeaders: ['Content-Type', 'Authorization'], 
+
 }));
 app.use(Routes);
 app.get('/', (req, res) => {
