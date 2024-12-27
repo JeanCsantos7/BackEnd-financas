7e18f4c91d23f6ebd9ed24d071eccdba6cd564be
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-26 22:08:55 -0300

    ra

--- app.ts
+++ app.ts
@@ -6,18 +6,15 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://controlefinanceiro7.netlify.app',
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization',
-    );
-    next();
-});
+const corsOptions = {
+    origin: 'https://controlefinanceiro7.netlify.app', // Origem permitida
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true, // Se precisar de credenciais
+};
+
+// Aplicando o middleware CORS globalmente
+app.use(cors(corsOptions));
 
 app.use(Routes);
 app.get('/', (req, res) => {
