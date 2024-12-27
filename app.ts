5d479361210450afad73d6f84382c8eab9eac469
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-26 21:42:50 -0300

    deucerto

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: 'https://controlefinanceiro7.netlify.app/',
+        origin: 'https://controlefinanceiro7.netlify.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
         allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
     }),
