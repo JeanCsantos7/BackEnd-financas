8685c63f17d60711e3c96a1549d81ee5195dbdb2
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-26 21:34:58 -0300

    teslamodels3modelcushbistrem

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: 'https://backend-financas-mauve.vercel.app',
+        origin: 'https://controlefinanceiro7.netlify.app/',
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
         allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
     }),
