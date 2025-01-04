19ad8f2002fb64ba3a34f3ff9d286fdd0165326d
JeanCsantos7 <jean.carlos87678@gmail.com>
2025-01-04 13:11:12 -0300

    ajustes23

--- app.ts
+++ app.ts
@@ -8,33 +8,22 @@ app.use(express.json());
 
 // Configurações CORS
 const corsOptions = {
-    origin: [
-        'http://localhost:3000',
-        'https://controlefinanceiro7.netlify.app',
-    ], // Definindo as origens permitidas
+    origin: ['https://controlefinanceiro7.netlify.app'], // Definindo as origens permitidas
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
     credentials: true, // Se precisar de credenciais
 };
 
-// Aplicando o middleware CORS manualmente
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header(
-        'Access-Control-Allow-Origin',
-        'https://controlefinanceiro7.netlify.app',
-    );
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// Aplicando o middleware CORS com as opções
+app.use(cors(corsOptions));
 
-app.use(Routes);
-
+// Rota principal para teste
 app.get('/', (req, res) => {
     console.log('Rota inicial acessada');
     res.send('Hello World, chove em SP');
 });
+
+// Rotas da aplicação
+app.use(Routes);
 
 export default app;
